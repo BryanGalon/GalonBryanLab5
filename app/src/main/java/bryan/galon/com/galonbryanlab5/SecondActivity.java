8b011c23bdf1b32c01885b8a587a64d275a1968b
Bryan <2015081611@ust-ics.mygbiz.com>
2018-09-29 03:19:48 +0800

    4ITE

--- app/src/main/java/bryan/galon/com/galonbryanlab5/SecondActivity.java
+++ app/src/main/java/bryan/galon/com/galonbryanlab5/SecondActivity.java
@@ -6,24 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent i = new Intent(this, CustomService.class);
-        this.startService(i);
+        setContentView(R.layout.activity_second);
     }
 
     public void process (View v){
         Intent i=null, chooser=null;
-        if (v.getId() == R.id.button2){
-            i = new Intent(this, SecondActivity.class);
+        if (v.getId() == R.id.button1){
+            i = new Intent(this, MainActivity.class);
             startActivity(i);
-        } else if (v.getId() == R.id.map1){
+        } else if (v.getId() == R.id.map2){
             i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse("geo:14.6201702,121.0606454"));
+            i.setData(Uri.parse("geo:14.5497665,121.047487"));
             chooser = Intent.createChooser(i, "Select application");
             startActivity(chooser);
         }
